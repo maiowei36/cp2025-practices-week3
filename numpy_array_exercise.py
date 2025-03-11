f65f8f56471caac2e31f57e396dacf1875e8734f
Xianghua Li <37569717+sunflowerlily@users.noreply.github.com>
2025-03-11 16:43:23 +0800

    Create numpy_array_exercise.py
    Numpy数组操作练习学生答案模板

---
+++ numpy_array_exercise.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def find_power_with_six():
+    """
+    找出所有满足条件的i值(0<i<100)，使得2^i的最后一位数字是6
+    
+    返回:
+    numpy.ndarray: 包含所有满足条件的i值的数组
+    """
+    # TODO: 实现此函数
+    pass
+
+def first_ten_powers():
+    """
+    计算2的前10次幂
+    
+    返回:
+    numpy.ndarray: 包含2的0次幂到9次幂的数组
+    """
+    # TODO: 实现此函数
+    pass
+
+def create_frame_matrix():
+    """
+    创建一个10x10的框架矩阵，边框为1，内部为0
+    
+    返回:
+    numpy.ndarray: 10x10的框架矩阵
+    """
+    # TODO: 实现此函数
+    pass
+
+def create_number_matrix():
+    """
+    创建一个10x10的数字矩阵，边框为-1，内部为0-39的数字
+    
+    返回:
+    numpy.ndarray: 10x10的数字矩阵
+    """
+    # TODO: 实现此函数
+    pass
+
+def create_multiplication_matrix():
+    """
+    创建一个3×5的矩阵，其中A[i,j] = i × j
+    要求使用数组广播实现
+    
+    返回:
+    numpy.ndarray: 3×5的矩阵
+    """
+    # TODO: 实现此函数
+    pass
+
+def create_special_matrix():
+    """
+    创建一个特殊的11×11矩阵，其中每个元素都是1
+    对角线上的元素为[5,4,3,2,1,0,1,2,3,4,5]
+    要求使用数组广播实现
+    
+    返回:
+    numpy.ndarray: 11×11的矩阵
+    """
+    # TODO: 实现此函数
+    pass
